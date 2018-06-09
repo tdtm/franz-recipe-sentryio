ffba71f12f1b26d9a87e75c367c6d85a1b0d6c91
tdtm <thierry.dtm@gmail.com>
2018-06-09 17:37:40 +0100

    Initial commit

---
+++ webview.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (Franz, options) => {
+  /* TODO
+  function getMessages() {
+    let count = 0;
+    let indirectCount = 0;
+    let badge = document.querySelector('.w-notification__count').innerText;
+
+    if (badge && badge.innerText) {
+      indirectCount = parseInt(badge.innerText);
+    }
+
+    Franz.setBadge(count, indirectCount);
+  }
+
+  Franz.loop(getMessages);
+  */
+};
